95207396d80278ce2212b6b2a8e5a44e5b4d2de6
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-01-05 06:24:44 -0300

    chore: Change structure and update README

--- data-structures/binaryTree.js
+++
@@ -1,1 +0,0 @@
-//A tree whose elements have at most 2 children is called a binary tree. Since each element in a binary tree can have only 2 children, we typically name them the left and right child.
